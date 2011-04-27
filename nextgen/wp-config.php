f0f89904f9469b3cdbf21525516f7ad343c53115
nikkhilnarang <congthang8924@gmail.com>
2011-04-27 12:08:16 +0000

    Updated with Server DB configs commented

--- nextgen/wp-config.php
+++ nextgen/wp-config.php
@@ -17,12 +17,15 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'nextgen');
+//define('DB_NAME', 'acenik_ngg');
 
 /** MySQL database username */
 define('DB_USER', 'root');
+//define('DB_USER', 'acenik_ngg');
 
 /** MySQL database password */
 define('DB_PASSWORD', '');
+//define('DB_PASSWORD', '5TDD9oG2vVVG');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
