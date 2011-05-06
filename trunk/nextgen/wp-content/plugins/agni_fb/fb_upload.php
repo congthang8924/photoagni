382aa81ffc7d35b31a58b1250466c41f0a647bb2
nikkhilnarang@gmail.com <nikkhilnarang@gmail.com@b9baaa19-de1e-55a5-0d8d-8e8a19608fec>
2011-05-06 23:18:45 +0000

    Modified Fb upload process to refer fb_token in the user_meta table this can be further used in the API

--- trunk/nextgen/wp-content/plugins/agni_fb/fb_upload.php
+++ trunk/nextgen/wp-content/plugins/agni_fb/fb_upload.php
@@ -6,19 +6,22 @@ if(!empty($gallery_params)){
 include('../../../wp-blog-header.php');
 header("HTTP/1.1 200 OK");
 include_once(WINABSPATH.'/wp-content/plugins/agni_fb/fbmain.php');
+  $decrypted = decrypt($gallery_params,'1a2g3n4i5');
+  $decrypted = explode('*',$decrypted);
+  
+  $filecaption = $decrypted[1];
+  $filepath = $decrypted[0];
+  $userID = $decrypted[2];
+  $fb_token = get_user_meta($userID,'fb_token',true);
 
-  if($fbme){
-    $decrypted = decrypt($gallery_params,'1a2g3n4i5');
-    $decrypted = explode('*',$decrypted);
-    
-    $filecaption = $decrypted[1];
-    $filepath = $decrypted[0];
-    
+  if(!empty($fb_token)){
+     
     $photos = array('0'=>$filepath);
     
     foreach($photos as $key=>$value){
     	$uploadstatus = $facebook->api("/me/photos", 'post',
     	array('source'  =>  '@'.$photos[$key],
+          'access_token' => $fb_token, 
     		  'message' => $filecaption));
     	if(!empty($uploadstatus['id'])){
         echo '<pre>Success: Image Uploaded to Facebook!</pre>';
