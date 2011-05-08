be0691f0d907d5dc831ca23609963447c3e461d4
idevelopement <congthang8924@gmail.com>
2011-05-08 16:28:17 +0000

    Auto FB changes in API done.

--- nextgen/apiform.php
+++ nextgen/apiform.php
@@ -1,6 +1,7 @@
 <form enctype="multipart/form-data" action="http://localhost/nextgen/api.php" method="post">
 Username: <input type="text" name="username"><br />
 Device: <input type="text" name="galleryselect"><br />
-Image <input type="file" name="imagefiles[]">
+Image <input type="file" name="imagefiles[]"><br />
+<input type="checkbox" name="auto_fb" value="auto_fb" /> Upload to Facebook<br />
 <input type="submit" name="submit">
 </form>
